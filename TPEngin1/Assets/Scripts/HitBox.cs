1d00b5b8d8ca7c7f577fc8077cca2ee98dc89297
BDEB\0769267 <p.montplaisir-m@hotmail.com>
2023-10-12 16:58:29 -0400

    End of class commit

--- TPEngin1/Assets/Scripts/HitBox.cs
+++ TPEngin1/Assets/Scripts/HitBox.cs
@@ -16,3 +16,57 @@ public class HitBox : MonoBehaviour
         }            
     }
 }
+
+/*
+ * Il mettrait ça dans une librairie
+ * PMM_Hitbox si dans une librairie
+ Script hitbox du prof
+
+[Serializefield]
+protected bool m_canHit;
+[Serializefield]
+protected bool m_canReceiveHit;
+//Si on veut une information seulement quand deux bool sont vrai, pas obligé
+protected bool CanGiveAndReceiveHits {get{return m_canHit && m_canReceiveHits;}}
+ 
+ [Serializefield]
+protected EAgentType m_agentType = EAgentType.Count;
+[Serializefield]
+protected list<EAgentType> m_affectedAgentTypes = new List<EAgentType> 
+ 
+ ^rivate void On triggerEnter(Collider other)
+
+var otherHitbox = other.GetComponent<MFHitbox>();
+if(otherhitbox = null) {return;}
+
+//Other collider also is MFHItbox
+// Le plusn intéressant devrait de mettre la logique d'agression ferait plus de sens
+if(m_canHit && otherHitBox. m_canReceiveHits)
+{
+if (m_afectedAgentsTypes.Contains(otherHitBox.m_agentType))
+{
+    //effectuer le hit
+//Et on peut transféer ca dans une autre méthode avec return true ou false
+la méthode peut être CanHitOther
+ensuite au retour de la fonction id CanHitOther on retourne true
+}
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
+/*
+ 
+ public enum EAgentType
+
+Ally,
+Enemy,
+Neutral,
+Count
+ 
+ */
